fe081474554f2f76be2ef3c79d0d085eec98be1a
Olli <olli17@gmx.net>
2017-09-13 16:00:44 +0200

    * superfluos code removed

--- forms/ProtectedOptionWizard.php
+++ forms/ProtectedOptionWizard.php
@@ -75,38 +75,6 @@ class ProtectedOptionWizard extends \OptionWizard
     public function generate()
     {
         $arrButtons = array('copy', 'drag', 'up', 'down', 'delete');
-        $strCommand = 'cmd_' . $this->strField;
-
-        // Change the order
-        if (\Input::get($strCommand) && is_numeric(\Input::get('cid')) && \Input::get('id') == $this->currentRecord)
-        {
-            $this->import('Database');
-
-            switch (\Input::get($strCommand))
-            {
-                case 'copy':
-                    array_insert($this->varValue, \Input::get('cid'), array($this->varValue[\Input::get('cid')]));
-                    break;
-
-                case 'up':
-                    $this->varValue = array_move_up($this->varValue, \Input::get('cid'));
-                    break;
-
-                case 'down':
-                    $this->varValue = array_move_down($this->varValue, \Input::get('cid'));
-                    break;
-
-                case 'delete':
-                    $this->varValue = array_delete($this->varValue, \Input::get('cid'));
-                    break;
-            }
-
-            $this->Database->prepare("UPDATE " . $this->strTable . " SET " . $this->strField . "=? WHERE id=?")
-                ->execute(serialize($this->varValue), $this->currentRecord);
-
-            $this->redirect(preg_replace('/&(amp;)?cid=[^&]*/i', '', preg_replace('/&(amp;)?' . preg_quote($strCommand, '/') . '=[^&]*/i', '', \Environment::get('request'))));
-        }
-
         // Make sure there is at least an empty array
         if (!is_array($this->varValue) || !$this->varValue[0])
         {
